f599dd37bf93745663e3f184e27b08122ec94d9a
TSergey123 <46027800+TSergey123@users.noreply.github.com>
2021-09-07 15:24:17 +0300

    experience fault

--- js/data/experienceData.js
+++ js/data/experienceData.js
@@ -1,6 +1,6 @@
 export const experienceData = [
   { jobTitle: 'Рсхб-интех', jobPosition: 'Руководитель направления автоматизации', jobTerm: 'октябрь 2019 - по настоящее время', jobImage: 'rshb.png' },
-  { jobTitle: 'Технопрогресс', jobPosition: 'Старший инженер по тестированию', jobTerm: 'апрель 2017 - октябрь 2019', jobImage: 'technoprogress.png' },
+  { jobTitle: 'Технопрогресс', jobPosition: 'Старший инженер по автоматизации', jobTerm: 'апрель 2017 - октябрь 2019', jobImage: 'technoprogress.png' },
   { jobTitle: 'Тинькофф страхование', jobPosition: 'Инженер по тестированию', jobTerm: 'февраль 2017 - апрель 2017', jobImage: 'tinkoff.png' },
   { jobTitle: 'Сбербанк технологии', jobPosition: 'Тестировщик ПО', jobTerm: 'июль 2016 - январь 2017', jobImage: 'sberbank.png' },
   { jobTitle: 'МТС', jobPosition: 'Младший тестировщик', jobTerm: 'март 2016 - июль 2016', jobImage: 'mts.png' }
